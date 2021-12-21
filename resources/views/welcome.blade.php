b2962927209f86cad9527580ed1dc562c8a3aa04
asantibanez <santibanez.andres@gmail.com>
2021-12-20 22:13:25 -0500

    Broadcasting working

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -128,5 +128,7 @@
                 </div>
             </div>
         </div>
+
+    <script src="{{ mix('js/app.js') }}"></script>
     </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Events\OrderShipmentStatusUpdated;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/broadcast', function () {
+    OrderShipmentStatusUpdated::broadcast();
+    return \Carbon\Carbon::now()->toIso8601String();
 });
